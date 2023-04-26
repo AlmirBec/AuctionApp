4d479abd98ac1f51e40596b6ca2d0022d76b4f35
Almir Becirovic <almircodeb@gmail.com>
2023-04-26 19:08:23 +0200

    Project skeleton

---
+++ auctionapp-be/src/main/java/com/example/auctionappbe/AuctionappBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionappBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionappBeApplication.class, args);
+	}
+
+}
---
+++ auctionapp-be/src/test/java/com/example/auctionappbe/AuctionappBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctionappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionappBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
